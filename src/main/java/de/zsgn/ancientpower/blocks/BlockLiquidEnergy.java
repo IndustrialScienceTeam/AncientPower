ff7eb2455345880e17d0f7f58e36f55149649efc
Lukas Jansen <lukas.jansen1997@yahoo.de>
2015-08-15 13:05:47 +0200

    Made liquid energy a finite gas

--- src/main/java/de/zsgn/ancientpower/blocks/BlockLiquidEnergy.java
+++ src/main/java/de/zsgn/ancientpower/blocks/BlockLiquidEnergy.java
@@ -2,18 +2,20 @@ package de.zsgn.ancientpower.blocks;
 
 import de.zsgn.ancientpower.AncientPower;
 import de.zsgn.ancientpower.fluids.FluidLiquidEnergy;
+import de.zsgn.ancientpower.proxies.CommonProxy;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.BlockFluidFinite;
 import net.minecraftforge.fluids.Fluid;
 
-public class BlockLiquidEnergy extends BlockFluidClassic {
+public class BlockLiquidEnergy extends BlockFluidFinite {
     public static final BlockLiquidEnergy instance=new BlockLiquidEnergy();
     public static final String NAME="Block"+FluidLiquidEnergy.NAME;
 
     public BlockLiquidEnergy() {
         super(FluidLiquidEnergy.instance, Material.water);
         this.setUnlocalizedName(AncientPower.MODID+"."+NAME);
+        
     }
 
 }
--- src/main/java/de/zsgn/ancientpower/fluids/FluidLiquidEnergy.java
+++ src/main/java/de/zsgn/ancientpower/fluids/FluidLiquidEnergy.java
@@ -3,6 +3,7 @@ package de.zsgn.ancientpower.fluids;
 import de.zsgn.ancientpower.AncientPower;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
 
 public class FluidLiquidEnergy extends Fluid {
 public static final String NAME="liquid_energy";
@@ -10,11 +11,10 @@ public static final FluidLiquidEnergy instance = new FluidLiquidEnergy();
 
     public FluidLiquidEnergy() {
         super(NAME, new ResourceLocation(AncientPower.MODID, NAME+"_still"), new ResourceLocation(AncientPower.MODID, NAME+"_flow"));
-       this.setLuminosity(20);
-       this.setDensity(500);
-       this.setViscosity(500);
+       this.setLuminosity(30);
+       this.setDensity(-1000);
+       this.setGaseous(true);
        this.setUnlocalizedName(AncientPower.MODID+"."+NAME);
     }
-
 
 }
