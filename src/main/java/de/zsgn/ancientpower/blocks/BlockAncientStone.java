74f66325ee3e3aa0a6511a656e2271aa2695cfd4
Baerenstark2 <fischgeier@yahoo.de>
2015-08-19 20:22:48 +0200

    versuchte Dropänderung

--- src/main/java/de/zsgn/ancientpower/blocks/BlockAncientStone.java
+++ src/main/java/de/zsgn/ancientpower/blocks/BlockAncientStone.java
@@ -1,21 +1,35 @@
 package de.zsgn.ancientpower.blocks;
 
+import java.util.Random;
+
 import de.zsgn.ancientpower.AncientPower;
+import de.zsgn.ancientpower.items.ItemChargeableCrystal;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.item.ItemStack;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 
 public class BlockAncientStone extends Block {
 	public final static String NAME="ancientstone";
-	public final static BlockAncientStone INSTANCE=new BlockAncientStone();
+	public final static BlockAncientStone INSTANCE=new BlockAncientStone(Item.getItemFromBlock(BlockBrokenAncientStone.INSTANCE));
 	
-	public BlockAncientStone() {
+	private int meta;
+	private Item drop;
+	
+	
+	public BlockAncientStone(Item drop) {
 		super(Material.rock);
+		this.drop = drop;
 		this.setUnlocalizedName(AncientPower.MODID+"."+NAME);
 		this.setCreativeTab(AncientPower.proxy.creativeTab);
 		this.setHardness(5F);
 		this.setResistance(20F);
 		this.setHarvestLevel("pickaxe",2);
 	}
+	@Override
+	public Item getItemDropped(IBlockState blockstate, Random random, int fortune) {
+	    return this.drop;
+	}
 
+	
 }
