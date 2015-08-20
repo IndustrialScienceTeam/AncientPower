3659f93d9b9b443453180c1421a2f370622e5c58
Baerenstark2 <fischgeier@yahoo.de>
2015-08-20 18:57:29 +0200

    deletet useless lines
    the stairs get by default the hardness and resistence of the block
    itself

--- src/main/java/de/zsgn/ancientpower/blocks/BlockAncientBrickStairs.java
+++ src/main/java/de/zsgn/ancientpower/blocks/BlockAncientBrickStairs.java
@@ -13,9 +13,7 @@ public class BlockAncientBrickStairs extends BlockStairs {
 		super(BlockAncientBricks.INSTANCE.getDefaultState());
 		this.setUnlocalizedName(AncientPower.MODID+"."+NAME);
 		this.setCreativeTab(AncientPower.proxy.creativeTab);
-		this.setHardness(5F);
-		this.setResistance(20F);
-		this.setHarvestLevel("pickaxe",2);
+		
 	}
 
 }
