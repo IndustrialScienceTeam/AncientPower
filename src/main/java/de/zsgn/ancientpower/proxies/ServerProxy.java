98b040770a43edc598ffec46d20309a6a4877237
Lukas Jansen <lukas.jansen1997@yahoo.de>
2015-08-20 17:54:55 +0200

    Added the proxy for the server

--- src/main/java/de/zsgn/ancientpower/proxies/ServerProxy.java
+++ src/main/java/de/zsgn/ancientpower/proxies/ServerProxy.java
@@ -7,19 +7,19 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class ServerProxy extends CommonProxy {
     @Override
     public void preInit(FMLPreInitializationEvent e) {
-        // TODO Auto-generated method stub
+       super.preInit(e);
 
     }
 
     @Override
     public void init(FMLInitializationEvent e) {
-        // TODO Auto-generated method stub
+      super.init(e);
 
     }
 
     @Override
     public void postInit(FMLPostInitializationEvent e) {
-        // TODO Auto-generated method stub
+        super.postInit(e);
 
     }
 
