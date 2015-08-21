ae0998472bb86fa258f8bae27ee78f225020e427
Lukas Jansen <lukas.jansen1997@yahoo.de>
2015-08-21 22:44:07 +0200

    Removed Debug code

--- src/main/java/de/zsgn/ancientpower/dimension/worldgen/SimpleWorldGen.java
+++ src/main/java/de/zsgn/ancientpower/dimension/worldgen/SimpleWorldGen.java
@@ -14,6 +14,7 @@ import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
 public class SimpleWorldGen implements IWorldGenerator {
@@ -33,7 +34,6 @@ public class SimpleWorldGen implements IWorldGenerator {
                     z=random.nextInt(16); 
                     y=MathHelper.clamp_int(y, 5, AncientPowerChunkProvider.MAXY);
                     new WorldGenMinable(blockgen.blockstate, blockgen.blockamount, replaces).generate(world, random, new BlockPos(x, y, z));
-                    System.out.println("Generated at "+new BlockPos(x, y, z));
                 }
             }
         }
